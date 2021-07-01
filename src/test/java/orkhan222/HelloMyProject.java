efe45bbded40c2457264fdd5847899d92693a7b2
Orkhan <jkrogers222@gmail.com>
2021-06-30 21:37:00 -0400

    first

--- src/test/java/orkhan222/HelloMyProject.java
+++ src/test/java/orkhan222/HelloMyProject.java
@@ -3,5 +3,7 @@ package orkhan222;
 public class HelloMyProject {
     public static void main(String[] args) {
         System.out.println("Project 2021/7");
+        //run
+        //first
     }
 }
